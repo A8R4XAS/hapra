fab32a3bc10d84fa40945ada607dbea116b8cc78
marlon <=>
2021-11-12 13:26:25 +0100

    add missing comonents and body

--- Blatt_04/4.1/c_d/mux81.vhdl
+++ Blatt_04/4.1/c_d/mux81.vhdl
@@ -28,8 +28,62 @@ ARCHITECTURE rtl OF mux81 IS
         );
     END COMPONENT;
 
-begin
+    COMPONENT andgate_2 IS
+        PORT (
+              input1     : IN std_logic;
+              input2     : IN std_logic;
+              and_2_result : OUT std_logic
+        );
+    END COMPONENT;
 
-    y_81 <= --missing;
+    
+    COMPONENT orgate_2 is
+        PORT (
+              input1    : IN std_logic;
+              input2    : IN std_logic;
+              or_2_result : OUT std_logic
+        );
+    END COMPONENT;
 
-end rtl;
+
+    COMPONENT notgate is
+        PORT (
+              input1     : IN std_logic;
+              not_result : OUT std_logic
+        );
+    END COMPONENT;
+
+    --signals
+    signal not_sel2 : std_logic;
+    signal sel41    : std_logic_vector(1 DOWNTO 0);
+
+    signal mux_out1 : std_logic_vector(2 DOWNTO 0);
+    signal mux_out2 : std_logic_vector(2 DOWNTO 0);
+
+    signal and_out1 : std_logic_vector(2 DOWNTO 0);
+    signal and_out2 : std_logic_vector(2 DOWNTO 0);
+    signal or_out   : std_logic_vector(2 DOWNTO 0);
+
+BEGIN
+
+    sel41(0) <= sel_81(0);
+    sel41(1) <= sel_81(1);
+
+    tmp : notgate port map(input1 => sel_81(2), not_result => not_sel2);
+
+    mux_output1 : mux41 port map(i1 => i1_81, i2 => i2_81, i3 => i3_81, i4 => i4_81, sel => sel41, y => mux_out1);
+
+    mux_output2 : mux41 port map(i1 => i5_81, i2 => i6_81, i3 => i7_81, i4 => i8_81, sel => sel41, y => mux_out2);
+
+    and_or_gate_assignment : for i in 0 to 2 generate
+
+        and_output1 : andgate_2 port map (input1 => mux_out1(i), input2 => not_sel2,  and_2_result => and_out1(i));
+        and_output2 : andgate_2 port map (input1 => mux_out2(i), input2 => sel_81(2), and_2_result => and_out2(i));
+
+        or_output : orgate_2 port map (input1 => and_out1(i), input2 => and_out2(i), or_2_result => or_out(i));
+
+    end generate and_or_gate_assignment;
+    
+    y_81 <= or_out;
+
+END rtl;
